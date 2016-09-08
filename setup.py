6e1be6976e1d77f971058376301eca511b0cdd22
Cenk Alti <cenkalti@gmail.com>
2016-09-08 14:53:45 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+# coding=utf8
+from setuptools import setup
+
+
+setup(
+    name='Darbe',
+    version='1.0.0',
+    author=u'Cenk Altı',
+    author_email='cenkalti@gmail.com',
+    keywords='mysql rds migration database replication slave',
+    url='https://github.com/cenk/darbe',
+    py_modules=['darbe'],
+    install_requires=[
+        'boto3',
+        'mysql-connector',
+    ],
+    description='RDS MySQL replication setup tool',
+    entry_points={
+        'console_scripts': [
+            'darbe = darbe',
+        ],
+    },
+)
